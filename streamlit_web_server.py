80d7abdd474999e4be38eb8926b7ed2c30286ecc
diedre.carmo <diedre.carmo@neuralmind.ai>
2025-03-02 13:00:52 -0300

    Initial implementation MEDPSeg online deploy

--- streamlit_web_server.py
+++ streamlit_web_server.py
@@ -1,10 +1,8 @@
 '''
 Entry point for cloud hosted streamlit server
 '''
-import subprocess
 from medpseg.streamlit_server import streamlit_server
 
 
 if __name__ == "__main__":
-    subprocess.run(["pip" , "install", "medpseg"])
     streamlit_server()

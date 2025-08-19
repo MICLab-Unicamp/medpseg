ea2c74f8db8c5a6ba45b1f4e6ec3ea578c41b868
diedre.carmo <diedre.carmo@neuralmind.ai>
2025-08-19 18:46:23 -0300

    Fix DB location on production

--- streamlit_web_server.py
+++ streamlit_web_server.py
@@ -7,6 +7,7 @@ from medpseg.streamlit_server import streamlit_server
 
 
 if __name__ == "__main__":
-    TINYDB_PATH = os.path.join("db", "medpseg_db.json")
+    TINYDB_PATH = os.path.join(os.environ["HOME"], "medpseg", "db", "medpseg_db.json")
+    os.makedirs(os.path.dirname(TINYDB_PATH), exist_ok=True)
     db = TinyDB(TINYDB_PATH)
     streamlit_server(db)
